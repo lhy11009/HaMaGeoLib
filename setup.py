36e2c8a370ef675663949ec3c6cee7baadc7d9bb
Haoyuan Li <hylli@ucdavis.edu>
2024-11-16 14:55:36 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here, e.g., 'numpy', 'pandas'
+    ],
+    author='Your Name',
+    description='A brief description of your package',
+    url='https://github.com/yourusername/my_package',
+    license='MIT',
+)
