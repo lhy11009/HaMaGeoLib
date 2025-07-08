71bdaa31b85e12bbbaa66e90c6840f8d05965d3c
Haoyuan Li <hylli@ucdavis.edu>
2025-07-08 16:21:48 -0700

    update slurm template on hive

--- scripts/server/hive/template_slurm.sh
+++ scripts/server/hive/template_slurm.sh
@@ -9,9 +9,21 @@
 #SBATCH --mail-type=ALL              # Notify user by email on all events
 #SBATCH --mail-user=hylli@ucdavis.edu # Email address for notifications
 
-#SBATCH --output=job_%j.log # Redirect standard output to file
-#SBATCH --error=job_%j.log   # Redirect standard error to file
+#SBATCH --output=job_%j.stdout # Redirect standard output to file
+#SBATCH --error=job_%j.stderr  # Redirect standard error to file
 
-set -x  # Enable debug mode (prints commands before executing)
 
+# for compile aspect
+## Enable debug mode (prints commands before executing)
+# set -x  
+## exclusive use
+#SBATCH --exclusive                  # Exclusive use of node (no sharing with other jobs)
+## deal.ii 9.6.1
+# /quobyte/billengrp/Software/deal.ii/deal.ii-9.6.1-toolchain-gcc-13.2.0-openmpi5.0.5
+## ASPECT
+# /quobyte/billengrp/lochy/Software/aspect
+## WorldBuilder
+# /quobyte/billengrp/lochy/Software/WorldBuilder
+
+# for running aspect
 srun /quobyte/billengrp/lochy/Software/aspect/build_master_TwoD_rebase/aspect-release ./case.prm
