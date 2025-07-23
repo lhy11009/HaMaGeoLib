07970c62b1c223122f634cc0061349f4ef9a0142
Haoyuan Li <hylli@ucdavis.edu>
2025-07-23 09:21:09 -0700

    Add noteboke for the Mass_Converving cases

---
+++ hamageolib/research/haoyuan_3d_subduction/scripts/SlabMorphology_2d.py
@@ -0,0 +1,32 @@
+import os, sys
+
+HaMaGeoLib_DIR = "/home/lochy/ASPECT_PROJECT/HaMaGeoLib"
+if os.path.abspath(HaMaGeoLib_DIR) not in sys.path:
+    sys.path.append(os.path.abspath(HaMaGeoLib_DIR))
+
+from hamageolib.research.haoyuan_3d_subduction.case_options import CASE_OPTIONS_TWOD1
+from hamageolib.research.haoyuan_3d_subduction.post_process import PlotCaseRunTwoD1, ProcessVtuFileTwoDStep
+from hamageolib.research.haoyuan_2d_subduction.workflow_scripts import run_2d_subduction_visualization
+
+
+local_dir_2d = "/mnt/lochz/ASPECT_DATA/TwoDSubduction/EBA_CDPT_3dconsistent_chunk_1/eba_cdpt_coh300_SA80.0_OA40.0_width80_ss500.0"
+
+assert(local_dir_2d is not None)
+
+# case options 
+Case_Options_2d = CASE_OPTIONS_TWOD1(local_dir_2d)
+Case_Options_2d.Interpret()
+Case_Options_2d.SummaryCaseVtuStep(os.path.join(local_dir_2d, "summary.csv"))
+
+graphical_steps_np = Case_Options_2d.summary_df["Vtu step"].to_numpy()
+graphical_steps = [int(step) for step in graphical_steps_np]
+
+# Processing pyvista
+for step in graphical_steps:
+    pvtu_step = step + int(Case_Options_2d.options['INITIAL_ADAPTIVE_REFINEMENT'])
+    output_dict = ProcessVtuFileTwoDStep(local_dir_2d, pvtu_step, Case_Options_2d)
+    Case_Options_2d.SummaryCaseVtuStepUpdateValue("Slab depth", step, output_dict["slab_depth"])
+    Case_Options_2d.SummaryCaseVtuStepUpdateValue("Trench", step, output_dict["trench_center"])
+    Case_Options_2d.SummaryCaseVtuStepUpdateValue("Dip 100", step, output_dict["dip_100"])
+
+Case_Options_2d.SummaryCaseVtuStepExport(os.path.join(local_dir_2d, "summary.csv"))
