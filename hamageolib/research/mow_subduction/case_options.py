6772de2ebbcae3a407c6134ed465501a56749cc3
Haoyuan Li <hylli@ucdavis.edu>
2025-07-31 12:26:37 -0700

    move Mo_kinetics to notebooks/research/haoyuan_metastable_subduction/MOW.ipynb

--- hamageolib/research/mow_subduction/case_options.py
+++ hamageolib/research/mow_subduction/case_options.py
@@ -1,6 +1,6 @@
 from ...utils.case_options import ModelConfigManager
+from ..haoyuan_3d_subduction.case_options import CASE_OPTIONS_TWOD1
 
 # todo_config
-class MowModelConfigManager(ModelConfigManager):
-
-    pass
+class CASE_OPTIONS_TWOD(CASE_OPTIONS_TWOD1):
+   pass
